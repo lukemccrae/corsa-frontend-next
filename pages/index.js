0f9b797819c3d381eb748e91187ad83b7b75ad71
lukemccrae <lukemccrae@gmail.com>
2023-11-14 18:04:21 -0700

    index

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// pages/index.js
+const HomePage = () => {
+    return (
+      <div>
+        <h1>Welcome to my Next.js App!</h1>
+        <p>This is the home page content.</p>
+      </div>
+    );
+  };
+  
+  export default HomePage;
