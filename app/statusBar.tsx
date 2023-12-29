6207f8b415835f53716fc24f910bc0ad20f1bfe8
lukemccrae <lukemccrae@gmail.com>
2023-12-29 13:06:55 -0700

    preview test

---
+++ app/statusBar.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+const poweredByStrava = "/api_logo_pwrdBy_strava_horiz_light.svg";
+
+interface StatusBarProps {
+  profile: string;
+}
+
+export const StatusBar = (props: StatusBarProps) => {
+  const StatusBarContainer = styled.div`
+    top: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #333;
+    color: #fff;
+    padding: 5px 50px;
+    width: 100%;
+  `;
+
+  const ProfilePicture = styled.img`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  `;
+
+  return (
+    <StatusBarContainer>
+      <h1
+        style={{ fontWeight: "bold", fontSize: "24px", padding: "0 10px 0 0" }}
+      >
+        CORSAAAAA
+      </h1>
+      <div style={{ display: "flex", alignItems: "baseline" }}>
+        <ProfilePicture src={props.profile} alt="Profile Picture" />
+        <img style={{ width: "100px" }} src={poweredByStrava} alt="My SVG" />
+      </div>
+    </StatusBarContainer>
+  );
+};
+
+export default StatusBar;
